f38a2d014b52fdd155d3320a93d7a9b554829875
Ryan Resma <rresma1999@gmail.com>
2020-08-21 14:06:24 -0500

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Ryan Resma on 8/21/20.
+//  Copyright © 2020 Ryan Resma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
