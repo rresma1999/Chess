5f4903e61aa86d9d005a73e591e900199ec1049b
rresma1999@gmail.com <rresma1999@gmail.com>
2020-08-21 16:16:01 -0500

    added implementations for Piece protocol and Dummy.swift

--- Chess/Dummy.swift
+++ Chess/Dummy.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import UIKit
+
+/*
+ Simple dummy class to act as a placeholder for an empty tile on the board
+ */
+class Dummy: Piece {
+    // Store the underlying coordinates
+    private var xStorage: CGFloat!
+    private var yStorage: CGFloat!
+    
+    // x coordinate
+    var x: CGFloat {
+        get {
+            return self.xStorage
+        }
+        set {
+            self.xStorage = newValue
+        }
+    }
+    
+    // y coordinate
+    var y: CGFloat {
+        get {
+            return self.yStorage
+        }
+        set {
+            self.yStorage = newValue
+        }
+    }
+    
+    // CGRect Constructor
+    init(frame: CGRect) {
+        self.xStorage = frame.origin.x
+        self.yStorage = frame.origin.y
+    }
+    
+    // Empty Constructor
+    init(){}
+}
