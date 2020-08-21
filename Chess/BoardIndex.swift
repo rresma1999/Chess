ae24b6d07947d42667d46f868e5d2ad670f58376
rresma1999@gmail.com <rresma1999@gmail.com>
2020-08-21 14:56:06 -0500

    Added skeleton for project and created chess board using Storyboard

---
+++ Chess/BoardIndex.swift
@@ -0,0 +1,9 @@
+//
+//  BoardIndex.swift
+//  Chess
+//
+//  Created by Ryan Resma on 8/21/20.
+//  Copyright © 2020 Ryan Resma. All rights reserved.
+//
+
+import Foundation
---
+++ Chess/Piece.swift
@@ -0,0 +1,14 @@
+//
+//  Piece.swift
+//  Chess
+//
+//  Created by Ryan Resma on 8/21/20.
+//  Copyright © 2020 Ryan Resma. All rights reserved.
+//
+
+import Foundation
+
+protocol Piece {
+    var x: CGFloat {get set}
+    var y: CGFloat {get set}
+}
