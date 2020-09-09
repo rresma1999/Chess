892a42015dc59b74fb51bbf421200766cb95e41a
rresma1999@gmail.com <rresma1999@gmail.com>
2020-09-08 23:18:04 -0500

    - implemented initializers for all the chess pieces

--- Chess/Queen.swift
+++ Chess/Queen.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+class Queen: UIChessPiece {
+    // constructor
+    init(frame: CGRect, color: UIColor, vc: ViewController) {
+        super.init(frame: frame)
+        if color == #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1) {
+            self.text = "♛"
+        } else {
+            self.text = "♕"
+        }
+        
+        self.isOpaque = false
+        self.textColor = color
+        self.isUserInteractionEnabled = true
+        self.textAlignment = .center
+        self.font = self.font.withSize(40)
+        
+        // add this king to our array of chess pieces
+        vc.chessPieces.append(self)
+        // render
+        vc.view.addSubview(self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
