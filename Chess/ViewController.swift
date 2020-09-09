7b842def9b91f91fe7f659b257139ca51ac56605
rresma1999@gmail.com <rresma1999@gmail.com>
2020-09-09 11:11:50 -0500

    - added the drag functionality to pieces as by overriding touchesBegan touchesMoved and touchesEnded functions

--- Chess/ViewController.swift
+++ Chess/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet var panGestureRecognizer: UIPanGestureRecognizer!
     
     var pieceDragged: UIChessPiece!
-    var srcOrigin: CGPoint!
+    var sourceOrigin: CGPoint!
     var destOrigin: CGPoint!
     // constant vars to help with instantiation of chess board
     static var SPACE_FROM_LEFT_EDGE: Int = 27
@@ -34,7 +34,35 @@ class ViewController: UIViewController {
         self.chessPieces = []
         self.myChessGame = ChessGame(viewController: self)
     }
+    
+    // triggered when piece is clicked or touched
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // only allow for user to drag chess pieces
+        pieceDragged = touches.first!.view as? UIChessPiece
+        if pieceDragged != nil {
+            self.sourceOrigin = pieceDragged.frame.origin
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if pieceDragged != nil {
+            drag(piece: pieceDragged, usingGestureRecognizer: self.panGestureRecognizer)
+        }
+    }
+    
+    // MARK: ensure that the piece's location is valid
+    // 1. make sure the center of the piece is in the center of the tile
+    // 2. make sure that it is a legal move?
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
 
-
+    // helper function to handle click and drag action
+    func drag(piece: UIChessPiece, usingGestureRecognizer gestureRecognizer: UIPanGestureRecognizer) {
+        let translation = gestureRecognizer.translation(in: view)
+        piece.center = CGPoint(x: translation.x + piece.center.x, y: translation.y + piece.center.y)
+        gestureRecognizer.setTranslation(CGPoint.zero, in: view)
+    }
+    
 }
 
