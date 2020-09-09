d06935afdf8589542d834eae3961c25bb9937ebd
rresma1999@gmail.com <rresma1999@gmail.com>
2020-09-08 23:58:23 -0500

    - created some useful variables for the ChessBoard class in order to help with instantiation of some of the pieces on the board - created the getFrame() function for ChessBoard in order to help instantiate all the needed pieces onto the exact pieces on the board

--- Chess/ChessBoard.swift
+++ Chess/ChessBoard.swift
@@ -10,5 +10,25 @@ import Foundation
 import UIKit
 
 class ChessBoard: NSObject {
+    var board: [[Piece]]!
+    var vc: ViewController!
+    let ROWS = 8
+    let COLS = 8
+    var whiteKing: King!
+    var blackKing: King!
     
+    func getFrame(forRow row: Int, forCol col: Int) -> CGRect {
+        // compute the x coordinate and y coordinate with some clever spacing
+        let x = CGFloat(ViewController.SPACE_FROM_LEFT_EDGE + col * ViewController.TILE_SIZE)
+        let y = CGFloat(ViewController.SPACE_FROM_TOP_EDGE + row * ViewController.TILE_SIZE)
+        
+        return CGRect(origin: CGPoint(x: x, y: y), size: CGSize(width: ViewController.TILE_SIZE, height: ViewController.TILE_SIZE))
+    }
+    
+    init(viewController: ViewController) {
+        vc = viewController
+        // initialize the board matrix with all dummy pieces first
+        let oneRowOfBoard = Array(repeating: Dummy(), count: COLS)
+        board = Array(repeating: oneRowOfBoard, count: ROWS)
+    }
 }
